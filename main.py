7e6cabfc920e093859fd79b31eae1d033c941fca
Rodrigo Molina <rodrigomd94@gmail.com>
2021-09-12 23:17:44 -0600

    added routes to get random nft and to check cardano backend api

--- main.py
+++ main.py
@@ -46,8 +46,23 @@ def get_address_for_specific_nft_sale(projectID, nft_id):
     response = requests.get(api_url)
     return jsonify(response.json())
 
+@app.route('/GetAddressForRandomNftSale/<string:projectID>', methods=['GET'])
+def get_address_for_random_nft_sale(projectID):
+    #price=config.prices_and_rarity[str(projectID)]["price"]
+    price=str(25000000)
+    #Temporarily using this api key. Remove this line later
+    #API_KEY= '72a5762cd58749db961901e4ddf41861' 
+    ################################
+    api_url = "https://api.nft-maker.io/GetAddressForRandomNftSale/" + API_KEY + "/" + projectID + "/1/"+price
+    print(api_url)
+    response = requests.get(api_url)
+    return jsonify(response.json())
+
 @app.route('/CheckAddress/<string:projectID>/<string:paymentAddress>', methods=['GET'])
 def CheckAddress(projectID, paymentAddress):
+    #Temporarily using this api key. Remove this line later
+    #API_KEY= '72a5762cd58749db961901e4ddf41861' 
+    ################################
     api_url = "https://api.nft-maker.io/CheckAddress/" + API_KEY + "/" + projectID + "/" + paymentAddress
     print(api_url)
     response = requests.get(api_url)
@@ -59,6 +74,24 @@ def get_project_price_and_rarity(projectID):
     toMint=config.prices_and_rarity[projectID]["toMint"]
     
     return jsonify({"price":price, "toMint":toMint})
+
+@app.route('/CheckCardanoAddress/<string:address>', methods=['GET'])
+def CheckCardanoAddress(address):
+    api_url = "https://cardano-mainnet.blockfrost.io/api/v0/addresses/"+str(address)+"/total"
+    print(api_url)
+    headers={'project_id':'cAiR6OBMC8HcwEc8kf9BVPR05eqW1Cx0'}
+    response = requests.get(api_url, headers=headers)
+    
+    return jsonify(response.json())
+
+@app.route('/CheckAsset/<string:asset>', methods=['GET'])
+def CheckAsset(asset):
+    api_url = "https://cardano-mainnet.blockfrost.io/api/v0/assets/"+str(asset)
+    print(api_url)
+    headers={'project_id':'cAiR6OBMC8HcwEc8kf9BVPR05eqW1Cx0'}
+    response = requests.get(api_url, headers=headers)
+    
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(debug=True)
